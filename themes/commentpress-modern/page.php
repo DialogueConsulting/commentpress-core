01f2f1bc8c0c412fe0fdce7b38251385f3c5badf
Hugh Stephens <hughstephens@hughstephens.com>
2014-06-14 17:40:48 +1000

    config for i18n

--- themes/commentpress-modern/page.php
+++ themes/commentpress-modern/page.php
@@ -271,7 +271,7 @@
 
 		<?php 
 		if ( function_exists( 'addthis_display_social_widget' ) ) {
-			_e( "Share this page:", 'commentpress-core' );
+			?><h5><?php _e( "Share this page:", 'commentpress-core' ); ?></h5><?php 
 			do_action( 'addthis_widget', get_permalink( $post ), get_the_title( $post ), 'below' );
 			}
 		?>
